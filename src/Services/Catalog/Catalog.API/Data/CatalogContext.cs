385a34ff715e62166d749cfd539992d30fae1e1d
julien <julienguillot@hotmail.fr>
2021-05-04 20:37:20 +0900

    Data Layer

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace Catalog.API.Data
 {
-    public class CatalogContext
+    public class CatalogContext : ICatalogContext
     {
-        public CatalogContext()
+        public CatalogContext(IConfiguration configuration)
         {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            CatalogContextSeed.SeedData(Products);
         }
+
+        public IMongoCollection<Product> Products { get; }
     }
 }
